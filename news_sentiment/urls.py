d943768063094d269277b912e7e24bf89d75b189
amaanz <134161288+amaanz@users.noreply.github.com>
2024-06-15 14:20:45 +0530

    Update urls.py

--- news_sentiment/urls.py
+++ news_sentiment/urls.py
@@ -22,3 +22,6 @@ urlpatterns = [
     path('', include('newsscore.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
